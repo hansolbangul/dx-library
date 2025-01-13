8e5da7a0663da7331a2ac0ad490a95b254310c68
hansolbangul <ruaenddl981028@naver.com>
2025-01-14 01:13:56 +0900

    [Feat] sns 추가

--- src/constants/snsLinks.ts
+++ src/constants/snsLinks.ts
@@ -1,8 +1,8 @@
 import { socialLinksSchema, type SocialLinks } from '@/schemas/sns.schema'
 
 export const SOCIAL_LINKS = socialLinksSchema.parse({
-  github: "https://github.com/yourusername",
-  linkedin: "https://linkedin.com/in/yourusername",
-  blog: "https://yourblog.com",
-  portfolio: "https://yourportfolio.com"
+  github: 'https://github.com/hansolbangul',
+  linkedin: 'https://www.linkedin.com/in/hansolbangul/',
+  blog: 'https://blog.hansolbangul.com/',
+  portfolio: 'https://profile.hansolbangul.com/',
 }) satisfies SocialLinks
