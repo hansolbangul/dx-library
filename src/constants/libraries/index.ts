1d851c186e487617b1542de2f3997f5a049839eb
hansolbangul <ruaenddl981028@naver.com>
2025-01-21 22:44:23 +0900

    [Feat] Modal 에 관한 설명 추가

--- src/constants/libraries/index.ts
+++ src/constants/libraries/index.ts
@@ -1,12 +1,12 @@
 import { librariesSchema } from '@/schemas/library.schema'
 import { generateRouter } from './generate-router'
-import { hooks } from './hooks'
+// import { hooks } from './hooks'
 import { modal } from './modal'
 
 const librariesData = {
   'generate-router': generateRouter,
   modal: modal,
-  hooks: hooks,
+  //   hooks: hooks,
 } as const
 
 export const LIBRARIES = librariesSchema.parse(librariesData)
